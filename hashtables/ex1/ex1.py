e801efb367c34386cc5de6669e1b7d4fc81853d8
DM <dm@DMs-MacBook-Pro.fios-router.home>
2020-05-08 12:54:26 -0400

    ex 1 passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,20 @@
 def get_indices_of_item_weights(weights, length, limit):
-
     """
     YOUR CODE HERE
     """
+    # create a dictionary for weights
+    weights_dict = {}
+
+    # loop to check if key value pair already in weights_dict
+    for i in range(length):
+        diff = limit - weights[i]
+
+        # if already in weights_dict
+        if weights_dict.get(diff) is not None:
+            # return i and retrieve diff
+            return (i, weights_dict.get(diff))
+        else:
+            # set key as with current value and value with index
+            weights_dict[weights[i]] = i
 
     return None
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,14 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    
     """
     YOUR CODE HERE
     """
+
+    # Make empty dictionary and empty variable to store final route values
+    # loop through each ticket
+    # set key with ticket source and value with destination
+    # initialize with destination of 'NONE'
+    # append destination to final route and reassign destination
 
     return route
